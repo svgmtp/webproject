48cd0d0b6b8052b60a5da1b28e039232e1e663f6
{user_github} <{email}>
2023-09-25 17:33:14 +0200

    todo

---
+++ testapp/views.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+def get_example(request):
+    response = {'request': {'time': datetime.now().isoformat(),
+                            'method': request.method,
+                            'path': request.path,
+                            'params': request.GET,
+                            'headers': dict(request.headers)}}
+    return JsonResponse(response, safe=False, json_dumps_params={'indent': 2})
+
+
+
+@csrf_exempt
+def post_example(request):
+    response = {'request': {'time': datetime.now().isoformat(),
+                'method': request.method,
+                'path': request.path,
+                'params': request.GET,
+                'headers': dict(request.headers),
+                'body': request.body.decode()}}
+    return JsonResponse(response, safe=False, json_dumps_params={'indent': 2})
--- webproject/urls.py
+++ webproject/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import testapp
 from django.contrib import admin
 from django.urls import path
+import testapp.views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('get_example', testapp.views.get_example),
+    path('post_example', testapp.views.post_example),
 ]
+
+
